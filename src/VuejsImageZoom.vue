2975102592a7ffdc8f0555904290ea57a3afa74d
kimjunhee <developer9101@gmail.com>
2024-01-12 00:56:33 +0900

    Add zoom button

--- src/VuejsImageZoom.vue
+++ src/VuejsImageZoom.vue
@@ -10,11 +10,23 @@
     @mouseleave="handleMouseUp"
   >
     <canvas :id="canvasId"></canvas>
+    <button class="image-zoom-icon" @click="onZoomIn()">
+      <img :src="ZoomIn" />
+    </button>
+    <button class="image-zoom-icon" @click="onZoomOut()">
+      <img :src="ZoomOut" />
+    </button>
+    <button class="image-zoom-icon" @click="onZoomReset()">
+      <img :src="ZoomReset" />
+    </button>
   </div>
 </template>
 
 <script>
 import { v4 as uuidv4 } from "uuid";
+import ZoomIn from "./assets/plus.svg";
+import ZoomOut from "./assets/minus.svg";
+import ZoomReset from "./assets/zoom-reset.svg";
 
 export default {
   name: "VuejsImageZoom",
@@ -22,6 +34,7 @@ export default {
     src: { type: String, default: "" },
     width: { type: String, default: "100%" },
     height: { type: String, default: "100%" },
+    enableButton: { type: Boolean, default: true },
   },
   watch: {
     src: {
@@ -42,9 +55,21 @@ export default {
         });
       },
     },
+    enableButton(isEnableButton) {
+      const canvasContainerEl = document.querySelector(`.vuejs-image-zoom`);
+      if (canvasContainerEl) {
+        const buttons = canvasContainerEl.getElementsByTagName("button");
+        Array.from(buttons).forEach((button) => {
+          button.style.display = isEnableButton ? "block" : "none";
+        });
+      }
+    },
   },
   data() {
     return {
+      ZoomIn: ZoomIn,
+      ZoomOut: ZoomOut,
+      ZoomReset: ZoomReset,
       canvasContainer: `canvas-container-${uuidv4()}`,
       canvasId: `canvas-id-${uuidv4()}`,
       canvasWidth: 0,
@@ -55,6 +80,7 @@ export default {
       lastY: 0,
       offsetX: 0,
       offsetY: 0,
+      maxScale: 10,
       minScale: 1,
       ctx: null,
       image: null,
@@ -96,8 +122,34 @@ export default {
         this.adjustOffset();
         this.drawImage();
       }
+
+      const canvasContainerEl = document.querySelector(`.vuejs-image-zoom`);
+      if (canvasContainerEl) {
+        canvasContainerEl.style.position = "relative";
+        const buttons = canvasContainerEl.getElementsByTagName("button");
+
+        Array.from(buttons).forEach((button, i) => {
+          const btnLeft =
+            this.canvasWidth / 2 - (buttons.length * 55) / 2 + i * 55;
+          button.style.display = this.enableButton ? "block" : "none";
+          button.style.width = "40px";
+          button.style.height = "40px";
+          button.style.position = "absolute";
+          button.style.bottom = "10px";
+          button.style.borderWidth = "0";
+          button.style.borderRadius = "50%";
+          button.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+          button.style.left = `${btnLeft}px`;
+          button.style.cursor = "pointer";
+
+          const icon = button.querySelector("img");
+
+          if (icon) {
+            icon.style.width = "20px";
+          }
+        });
+      }
     },
-    // 초기 이미지 크기(scale) 지정 및 한화면에 나올 수 있게 작업
     fitImageIntoCanvas() {
       // 화면보다 이미지가 클경우
       if (
@@ -171,9 +223,7 @@ export default {
       const zoom = Math.exp(wheel * scaleMultiplier);
       const nextScale = this.scale * zoom;
 
-      if (nextScale > 4 || nextScale < this.minScale) {
-        return;
-      }
+      if (nextScale > this.maxScale || nextScale < this.minScale) return;
 
       const offsetX = (x - this.offsetX) * (zoom - 1);
       const offsetY = (y - this.offsetY) * (zoom - 1);
@@ -219,6 +269,37 @@ export default {
         this.lastX = event.clientX;
         this.lastY = event.clientY;
       }
+    },
+    onZoomIn() {
+      this.zoom(1.2);
+    },
+    onZoomOut() {
+      this.zoom(0.8);
+    },
+    zoom(zoomMultiplier) {
+      const canvas = document.querySelector(`#${this.canvasId}`);
+      const centerX = canvas.width / 2;
+      const centerY = canvas.height / 2;
+
+      const nextScale = this.scale * zoomMultiplier;
+      if (nextScale > this.maxScale || nextScale < this.minScale) return;
+
+      const offsetX = (centerX - this.offsetX) * (zoomMultiplier - 1);
+      const offsetY = (centerY - this.offsetY) * (zoomMultiplier - 1);
+
+      this.offsetX -= offsetX;
+      this.offsetY -= offsetY;
+      this.scale *= zoomMultiplier;
+
+      this.adjustOffset();
+      this.drawImage();
+    },
+    onZoomReset() {
+      this.scale = this.minScale;
+      this.offsetX = 0;
+      this.offsetY = 0;
+      this.adjustOffset();
+      this.drawImage();
     },
   },
 };
